747f8b04500ff0a817d9143b7e3b559fd2dc829b
Vladislav Plotnikov <vladik1209@gmail.com>
2019-09-17 16:01:34 +0300

    update description

--- hello/main.cpp
+++ hello/main.cpp
@@ -6,7 +6,9 @@ int main(int args, char* argv[]) {
     Application app(args,argv); //create application
 
     MainWindow w; //create window
-    w.show();
+    w.show();  //на этом месте возникала ошибка SEGMENTATION FAULT
+    // путем недолгого гугления, ответ нашелся на stack overflow ()
 
+    //ошибка возникала из-за того, что argc передавалась в конструктор Application не по ссылке, а напрямую
     return app.exec();
 }
